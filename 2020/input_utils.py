a977e8b3e2dec9347cf1333a7bdef82102614e72
yamakanto <yamakantos@gmail.com>
2020-12-11 14:11:41 +0100

    add removal of nl to input utils

--- 2020/input_utils.py
+++ 2020/input_utils.py
@@ -1,8 +1,13 @@
 import os
 
+
 def get_input_file_lines(filename):
     rel_path = os.path.join('2020', 'inputs', filename)
     my_path = os.getcwd()
     path = os.path.join(my_path, rel_path)
     with open(path) as file:
         return file.readlines()
+
+
+def get_input_file_lines_no_nl(filename):
+    return [x.replace('\n', '') for x in get_input_file_lines(filename)]
